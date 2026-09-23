a1911b43d8a294f04925c62c36f19e953dea105a
Guilherme Costa <guilhermekuste@gmail.com>
2026-09-23 12:54:49 -0300

    adiciona tela login

---
+++ ProjetoPI/src/app/pages/produtos/acessorios/acessorios.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acessorios',
+  imports: [],
+  templateUrl: './acessorios.html',
+  styleUrl: './acessorios.css',
+})
+export class Acessorios {}
